00d54ea3ff2f19a850fd10d99825eac1d71a3b7f
LoubnaAm <163037692+LoubnaAm@users.noreply.github.com>
2025-03-08 21:36:27 +0000

    second commit

---
+++ src/connection/dbConnection.java
@@ -0,0 +1,24 @@
+package connection;
+
+
+
+
+public class dbConnection {
+   /* public static  Connection connecte() throws SQLException {
+        try {
+            String url = "";
+            String user = "root";
+            String password = "";
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection conn = DriverManager.getConnection(url, user, password);
+            System.out.println("Connxion reussite");
+            return conn;
+ 
+        } catch (ClassNotFoundException | SQLException e) {
+            Logger.getLogger(dbConnection.class.getName()).log(Level.SEVERE, null, e);
+            e.printStackTrace();
+            throw new SQLException("Erreur lors de la connexion � la base de donnees", e);
+        }
+    }
+*/
+}
